33454501e3101986b87a251f68b6e75c11a48c74
noah-williams <noahwilliams212@gmail.com>
2022-04-11 20:28:51 -0500

    Added type and divide by zero testing on one of the functions

--- hw5/test_functions.py
+++ hw5/test_functions.py
@@ -1,5 +1,6 @@
 import pytest
 from functions import *
 
-def test_numbers():
-    assert numbers(6, 3) == 2
+@pytest.mark.parametrize("value1,value2,sum", [(3, 0, 0), ("6", "2", 3)])
+def test_numbers(value1, value2, sum):
+    assert numbers(value1, value2) == sum
