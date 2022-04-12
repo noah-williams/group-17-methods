bae09da5f50b55079590ba3798ce2ce9d992862a
noah-williams <noahwilliams212@gmail.com>
2022-04-11 22:07:04 -0500

    Commented out the non-functional distance tests

--- hw5/test_functions.py
+++ hw5/test_functions.py
@@ -1,10 +1,14 @@
 import pytest
 from functions import *
 
-@pytest.mark.parametrize("file",["testing.txt", "testing"])
+@pytest.mark.parametrize("file",["testing.txt", "ThisFileDoesNotExist.txt"])
 def test_openFile(file):
     assert openFile(file) == None
 
-@pytest.mark.parametrize("value1,value2,sum", [(3, 0, 0), ("6", "2", 3), (-6, 2, 3)])
+@pytest.mark.parametrize("value1,value2,sum", [(3, 0, 0), ("6", "2", 3), (6, 3, 2), (10.4, 5.2, 2), (15, 7.5, 2), (-4, -2, 2)])
 def test_numbers(value1, value2, sum):
     assert numbers(value1, value2) == sum
+
+#@pytest.mark.parametrize("x1,y1,x2,y2,sum", [(2, 1, 4, ), ("6", "2", 3), (6, 3, 2), (10.4, 5.2, 2), (15, 7.5, 2), (-4, -2, 2)])
+#def test_numbers(value1, value2, sum):
+#    assert numbers(value1, value2) == sum
