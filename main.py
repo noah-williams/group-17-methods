96e4a45bc8365d9f66b791fc68f66ad9a9e7cb1c
Connor Hicks <connorhicks@outlook.com>
2022-04-27 10:35:49 -0500

    Start of the codebase

---
+++ main.py
@@ -0,0 +1,23 @@
+#login with username & password
+
+
+userInput = int(input("Please select an option 1-5: "))
+while 1 <= userInput <= 5:
+
+    #if user presses 1, show list of games
+
+
+    #if user presses 2, show their cart
+
+
+    #if user presses 3, show their account info
+
+
+    #if user presses 4, logout
+
+
+    #if user presses 5, exit
+    if userInput == 5:
+        quit()
+
+    userInput = int(input("Please select an option 1-5: "))
