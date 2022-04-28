e237bee3e3ee4888a90a4e016afd1d4d03fc0b23
Connor Hicks <connorhicks@outlook.com>
2022-04-28 18:09:31 -0500

    Postgres localhost database connection implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from goto import with_goto
+import psycopg2
 import Game
 import Cart
 import User
@@ -6,64 +7,79 @@ import Order
 
 
 @with_goto
-# Login with username & password
+def main():
+    # Login with username & password
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="testdb",
+            user="postgres",
+            password="<password goes here>")
+        cur = conn.cursor()
 
-label .begin
-print("1: Login")
-print("2: Create account")
-print("3: Quit program")
-userInput = int(input("Please select an option 1-3: "))
+        # label .begin
+        print("1: Login")
+        print("2: Create account")
+        print("3: Quit program")
+        user_input = int(input("Please select an option 1-3: "))
 
-while 1 <= userInput <= 3:
+        while 1 <= user_input <= 3:
 
-    # if user presses 1, have inputs for username and password, then check if they exist in database
-    if userInput == 1:
-        # todo
-        break
+            # if user presses 1, have inputs for username and password, then check if they exist in database
+            if user_input == 1:
+                # todo
+                break
 
-    # if user presses 2, have inputs to create a new user, then send them to the store
-    if userInput == 2:
-        # todo
-        break
+            # if user presses 2, have inputs to create a new user, then send them to the store
+            if user_input == 2:
+                # todo
+                break
 
-    if userInput == 3:
-        quit()
+            if user_input == 3:
+                quit()
 
-    print("1: Login")
-    print("2: Create account")
-    print("3: Quit program")
-    userInput = int(input("Please select an option 1-3: "))
+            print("1: Login")
+            print("2: Create account")
+            print("3: Quit program")
+            user_input = int(input("Please select an option 1-3: "))
+
+        # Once logged in, run store functionality
+
+        print("1: Show games")
+        print("2: Show cart")
+        print("3: Show account")
+        print("4: Logout")
+        print("5: Quit program")
+        user_input = int(input("Please select an option 1-5: "))
+        while 1 <= user_input <= 5:
+
+            if user_input == 1:
+                Game.view()
+
+            if user_input == 2:
+                Cart.view()
+
+            if user_input == 3:
+                User.view()
+
+            # if user presses 4, logout
+            if user_input == 4:
+                quit()
+                # goto .begin
+
+            if user_input == 5:
+                quit()
+
+            print("1: Show games")
+            print("2: Show cart")
+            print("3: Show account")
+            print("4: Logout")
+            print("5: Quit program")
+            user_input = int(input("Please select an option 1-5: "))
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
-# Once logged in, run store functionality
-
-print("1: Show games")
-print("2: Show cart")
-print("3: Show account")
-print("4: Logout")
-print("5: Quit program")
-userInput = int(input("Please select an option 1-5: "))
-while 1 <= userInput <= 5:
-
-    if userInput == 1:
-        Game.view()
-
-    if userInput == 2:
-        Cart.view()
-
-    if userInput == 3:
-        User.view()
-
-    # if user presses 4, logout
-    if userInput == 4:
-        goto .begin
-
-    if userInput == 5:
-        quit()
-
-    print("1: Show games")
-    print("2: Show cart")
-    print("3: Show account")
-    print("4: Logout")
-    print("5: Quit program")
-    userInput = int(input("Please select an option 1-5: "))
+if __name__ == "__main__":
+    main()
