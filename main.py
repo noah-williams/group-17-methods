386b7102c6adae945ae866c50ea37cc5cbfdc4d9
JacobG623 <97655350+JacobG623@users.noreply.github.com>
2022-04-29 17:08:17 -0500

    Eliminated goto in main.py

--- main.py
+++ main.py
@@ -1,49 +1,75 @@
-from goto import with_goto
 import psycopg2
 import Game
 import Cart
 import User
 import Order
 
+checkVar = False
 
-@with_goto
 def main():
     # Login with username & password
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="testdb",
-            user="postgres",
-            password="<password goes here>")
-        cur = conn.cursor()
+    if checkVar != True:
+        try:
+            conn = psycopg2.connect(
+                host="localhost",
+                database="testdb",
+                user="postgres",
+                password="<password goes here>")
+            cur = conn.cursor()
+        except (Exception, psycopg2.DatabaseError) as error:
+            
+    print(error)
+    # label .begin
+    print("1: Login")
+    print("2: Create account")
+    print("3: Quit program")
+    user_input = int(input("Please select an option 1-3: "))
 
-        # label .begin
+    while 1 <= user_input <= 3:
+
+        # if user presses 1, have inputs for username and password, then check if they exist in database
+        if user_input == 1:
+            # todo
+            break
+
+        # if user presses 2, have inputs to create a new user, then send them to the store
+        if user_input == 2:
+            # todo
+            break
+
+        if user_input == 3:
+            quit()
+
         print("1: Login")
         print("2: Create account")
         print("3: Quit program")
         user_input = int(input("Please select an option 1-3: "))
 
-        while 1 <= user_input <= 3:
+    # Once logged in, run store functionality
 
-            # if user presses 1, have inputs for username and password, then check if they exist in database
-            if user_input == 1:
-                # todo
-                break
+    print("1: Show games")
+    print("2: Show cart")
+    print("3: Show account")
+    print("4: Logout")
+    print("5: Quit program")
+    user_input = int(input("Please select an option 1-5: "))
+    while 1 <= user_input <= 5:
 
-            # if user presses 2, have inputs to create a new user, then send them to the store
-            if user_input == 2:
-                # todo
-                break
+        if user_input == 1:
+            Game.view()
 
-            if user_input == 3:
-                quit()
+        if user_input == 2:
+            Cart.view()
 
-            print("1: Login")
-            print("2: Create account")
-            print("3: Quit program")
-            user_input = int(input("Please select an option 1-3: "))
+        if user_input == 3:
+            User.view()
 
-        # Once logged in, run store functionality
+        # if user presses 4, logout
+        if user_input == 4:
+            main()
+
+        if user_input == 5:
+            quit()
 
         print("1: Show games")
         print("2: Show cart")
@@ -51,34 +77,6 @@ def main():
         print("4: Logout")
         print("5: Quit program")
         user_input = int(input("Please select an option 1-5: "))
-        while 1 <= user_input <= 5:
-
-            if user_input == 1:
-                Game.view()
-
-            if user_input == 2:
-                Cart.view()
-
-            if user_input == 3:
-                User.view()
-
-            # if user presses 4, logout
-            if user_input == 4:
-                quit()
-                # goto .begin
-
-            if user_input == 5:
-                quit()
-
-            print("1: Show games")
-            print("2: Show cart")
-            print("3: Show account")
-            print("4: Logout")
-            print("5: Quit program")
-            user_input = int(input("Please select an option 1-5: "))
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
 
 
 if __name__ == "__main__":
