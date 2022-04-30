ac83fc4ef80358430a976756706b6ab68e5a63de
JacobG623 <97655350+JacobG623@users.noreply.github.com>
2022-04-30 12:55:54 -0500

    Comment of potential implementation for display

--- Game.py
+++ Game.py
@@ -8,6 +8,7 @@ def view():
             return
 
         # todo: Show list of games that you can select
+        # Implementation Idea: Use Select Name from Games and add those to a list. Then use a for loop to iterate through the list and generate a menu which allows the user to select a game by giving the index number which then does a Select * From Games Where Name = list[i] 
 
         print("1: Go back")
         game_input = int(input("Please select an option: "))
