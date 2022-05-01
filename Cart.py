a02d8096d3978557a9e091480fcdd547d6e01cd1
JacobG623 <97655350+JacobG623@users.noreply.github.com>
2022-05-01 18:42:00 -0500

    Update Cart.py

--- Cart.py
+++ Cart.py
@@ -1,29 +1,40 @@
-def view():
-    print("1: Go back")
-    print("2: View cart")
-    print("3: Remove game")
-    print("4: Checkout")
-    cart_input = int(input("Please select an option 1-4: "))
+from sqlite3 import Row
+import main
+import Order
 
-    while 1 <= cart_input <= 4:
+def view(cursor):
+    while 1:
+
+        print("\n\n\n1: Go back")
+        print("2: View cart")
+        print("3: Remove game")
+        print("4: Checkout")
+        cart_input = int(input("\nPlease select an option 1-4: "))
 
         if cart_input == 1:
             return
 
         if cart_input == 2:
-            # todo: View games in cart
-            return
+            cursor.execute("SELECT * FROM cart WHERE username = '" + main.signed_in_username + "'")
+            row = cursor.fetchall()
+            print("\n\n\n" + row)
 
         if cart_input == 3:
-            # todo: View games in cart, then select a game to remove
-            return
+            cursor.execute("DELETE FROM cart WHERE username = '" + main.signed_in_username + "' AND title = '" + "'")
+            row = cursor.fetchone()
+            print(row)
 
         if cart_input == 4:
-            # todo: Remove games in cart, then lower games stock
-            return
-
-        print("1: Go back")
-        print("2: View cart")
-        print("3: Remove game")
-        print("4: Checkout")
-        cart_input = int(input("Please select an option 1-4: "))
+            cursor.execute("SELECT price FROM cart WHERE username = '" + main.signed_in_username + "'")
+            priceList = cursor.fetchall()
+            totalCost = 0
+            for price in priceList:
+                totalCost += price
+            cursor.execute("SELECT title FROM cart WHERE username = '" + main.signed_in_username + "'")
+            orderItems = cursor.fetchall()
+            cursor.execute("SELECT paymentInfo FROM cart WHERE username = '" + main.signed_in_username + "'")
+            paymentInfo = cursor.fetchone()
+            Order.new_order(totalCost, orderItems, paymentInfo, cursor)
+            cursor.execute("DELETE FROM cart WHERE username = '" + main.signed_in_username + "'")
+            row = cursor.fetchone()
+            print(row)
