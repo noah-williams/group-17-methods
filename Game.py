f321558ed7e715d6297162e807f81004290e70e2
JacobG623 <97655350+JacobG623@users.noreply.github.com>
2022-05-01 22:51:18 -0500

    Game with lower stock added

--- Game.py
+++ Game.py
@@ -18,15 +18,30 @@ def view(connection, cursor):
         if game_input == 1:
             return
 
-        elif game_input < (count_var - 1):
+        else:
             print("\n\n\nFull info for " + new_games[game_input - 2])
             game_title = str(new_games[game_input - 2])
             cursor.execute('SELECT * FROM games WHERE games.title = (%s)', (game_title,))
             game_data = cursor.fetchone()
-            labels = ["GameId", "Title", "Developer", "Publisher", "Genre", "Price", "Rating", "Inventory", "Rating", "Release Date"]
+            labels = ["GameId", "Title", "Developer", "Publisher", "Genre", "Price", "ESRB", "Inventory", "User Rating", "Release Date"]
             i = 0
             for x in game_data:
                 labels[i], ": ",
                 print(labels[i], ": ", x , sep='')
                 i += 1
             print("\n")
+
+def lower_stock(connection, cursor, title):
+    cursor.execute('SELECT stock FROM games WHERE games.title = (%s)', (title,))
+    row = cursor.fetchone()
+    for x in row:
+        temp = int(x)
+    print(temp)
+    stock = temp - 1
+    cursor.execute('UPDATE games SET stock = (%s) WHERE games.title = (%s)', (stock, title,))
+    connection.commit()
+    cursor.execute('SELECT stock FROM games WHERE games.title = (%s)', (title,))
+    row2 = cursor.fetchone()
+    for y in row2:
+        temp2 = int(y)
+    print(temp2)
