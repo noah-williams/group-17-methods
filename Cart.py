ddc4beb867c3a0be25706213f4fd58dce47d292a
noah-williams <noahwilliams212@gmail.com>
2022-05-02 14:27:30 -0500

    Cart changes

--- Cart.py
+++ Cart.py
@@ -1,5 +1,11 @@
 import main
 
-class Cart(self, userID):
+class Cart(self, userID, cursor):
+
+    cart_list = []
+
     def __init__(self, userID):
         self.userID = userID
+
+        cursor.execute("SELECT cartid FROM carts")
+        current_cartids = cursor.fetchall()
