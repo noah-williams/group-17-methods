70878fd93bee18d4fba672b443d9488fbb260ab1
JacobG623 <97655350+JacobG623@users.noreply.github.com>
2022-05-01 20:50:56 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,26 +1,25 @@
 import main
 import psycopg2
 
-def view(cursor):
-    # gameList = ["The Elder Scrolls V: Skyrim – Special Edition", "Elden Ring", "Cyberpunk 2077", "DEATHLOOP", "LEGO Star Wars: The Skywalker Saga", "Marvel’s Avengers", "Monster Hunter: World", "Stardew Valley", "Madden NFL 22", "Battlefield 2042"]
-    # cursor.execute("GRANT ALL PRIVILEGES ON TABLE games TO group17")
+def view(connection, cursor):
     cursor.execute("SELECT title FROM games")
     games = cursor.fetchall()
+    new_games = [item for t in games for item in t]
 
     while 1:
 
-        print("\n\n\n0: Go back")
-        count_var = 1
+        print("\n\n\n1: Go back")
+        count_var = 2
         for x in games:
             print(count_var, ": ", x, sep='')
             count_var += 1
-        game_input = int(input("\nPlease select an option: "))
+        game_input = int(input("\nPlease select an option 1-" + str(count_var) + ": "))
 
-        if game_input == 0:
+        if game_input == 1:
             return
 
         else:
-            print("Full info for " + games[game_input - 1])
+            print("\n\n\nFull info for " + new_games[game_input - 2])
             cursor.execute("SELECT * FROM games WHERE games.id = %d", game_input)
             game_data = list(cursor.fetchone())
             labels = ["GameId", "Title", "Developer", "Publisher", "Genre", "Price", "Rating", "Inventory", "Rating", "Release Date"]
